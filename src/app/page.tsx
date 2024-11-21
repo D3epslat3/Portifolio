162662705d136c79da852b8b480b3203c1539bf7
Miguel Sousa <miguel.msousa@outlook.com>
2024-11-20 22:08:03 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import Image from "next/image";
-import * as React from "react";
+import React from "react";
 import Typewriter from "typewriter-effect";
 
 export default function Home() {
